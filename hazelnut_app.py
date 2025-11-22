9fe23de6c81f15ef18c4bd065b050cfd1ee849b9
denizturdu <deniz@avella.se>
2025-11-22 13:40:47 +0300

    Update hazelnut_app.py

--- hazelnut_app.py
+++ hazelnut_app.py
@@ -95,7 +95,7 @@ else:
 
                 s1, s2, s3 = st.columns(3)
                 size_1 = s1.number_input("1 Numara (13mm üzeri) (%)", value=0.0)
-                under_size = s2.number_input("Elek Altı (9mm üzeri) (%)", value=0.0)
+                under_size = s2.number_input("Elek Altı (9mm altı) (%)", value=0.0)
                 moisture = s3.number_input("Rutubet (%)", 0.0, 20.0, 5.0)
                 
                 calc_pressed = st.form_submit_button("🔄 Randıman Hesapla")
